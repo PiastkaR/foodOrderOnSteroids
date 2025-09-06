cf17d0eac2910623526877fa8cf4108575d8bfdf
piastkar <rafal.piastka@contractors.roche.com>
2025-09-06 09:44:41 +0200

    common domain added

---
+++ common/common-domain/src/main/java/com/food/order/domain/event/DomainEvent.java
@@ -0,0 +1,4 @@
+package com.food.order.domain.event;
+
+public interface DomainEvent<T> {
+}
---
+++ common/common-domain/src/main/java/com/food/order/domain/valueobject/OrderId.java
@@ -0,0 +1,9 @@
+package com.food.order.domain.valueobject;
+
+import java.util.UUID;
+
+public abstract class OrderId extends BaseId<UUID> {
+    public OrderId(UUID value) {
+        super(value);
+    }
+}
